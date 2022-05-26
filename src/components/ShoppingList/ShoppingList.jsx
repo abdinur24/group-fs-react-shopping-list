32d2785596ab63e8da5d8dfbf2fd47bce98b9052
Brian Campbell <bry123ftw@gmail.com>
2022-05-26 16:07:59 -0500

    shopping list structure is worked out...?

---
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -0,0 +1,22 @@
+function ShoppingList({shopListArray}) {
+    console.log("in shopping list", shopListArray);
+    
+
+    return(
+        <>
+            <button id="resetButton">Reset</button>
+            <button id="clearButton">Reset</button>
+            <div>
+                <ul>
+                    {shopListArray.map((name, quantity, unit, purchased, index) => 
+                        <li key={index} >
+                            {name} {unit} {purchased}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default ShoppingList;
