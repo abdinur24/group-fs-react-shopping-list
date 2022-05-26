3f329f371b47831aa86284a5bc23898ae32e44ad
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2022-05-26 11:40:00 -0500

    databse made

--- database.sql
+++ database.sql
@@ -1,2 +1,17 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+DATABASE NAME- react-shopping-list
+
+CREATE TABLE shoppingcart(
+"id" serial primary key,
+"name"  varchar(80) NOT NULL,
+"quantity"  decimal(20) NOT NULL,
+"unit" varchar(20),
+"purchased" boolean default false)
+;
+
+SELECT * FROM shoppingcart;
+
+INSERT INTO "shoppingcart"("name","quantity","unit","purchased")
+VALUES('apple','2','lbs','false');
